463d0e430de5e21d4e0e0a9fb02557c24323409f
Christian Jay Tibi <145688353+christianjaytibi@users.noreply.github.com>
2024-08-20 11:31:49 +0800

    add handshake.cpp

---
+++ handshake.cpp
@@ -0,0 +1,23 @@
+/*
+  Given T number of persons in a room, count the number of minimum handshakes
+  that will occur if each person shook hands with everyone else at lease once.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  int a[n];
+
+  for (int i = 0; i < n; ++i) {
+    cin >> a[i];
+  }
+
+  for (int i = 0; i < n; ++i) {
+    cout << (a[i] - 1) * (static_cast<float>(a[i]) / 2) << "\n";
+  }
+
+  return 0;
+}
